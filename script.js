49958aa6d7dc94bdb49337188abaf392db40fb15
AartiW-12 <aartiwhaval12@gmail.com>
2025-10-18 13:44:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const openModalBtn = document.getElementById("openModal");
+const closeModal = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal")
+
+openModalBtn.addEventListener("click", ()=>{
+	modal.classList.remove("hidden");
+})
+
+closeModal.addEventListener("click", ()=>{
+	modal.classList.add("hidden")
+})
+
+window.addEventListener("click", (e)=>{
+	if(e.target === modal){
+		modal.classList.add("hidden")
+	}
+});
