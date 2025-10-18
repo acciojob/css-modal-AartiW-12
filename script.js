9fd020dd89f510cdb2b13fc63646f33a0cdfff7c
AartiW-12 <aartiwhaval12@gmail.com>
2025-10-18 13:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openModalBtn = document.getElementById("openModal");
 const closeModal = document.querySelector(".close-modal");
 const modal = document.querySelector(".modal")
+const modalContent = document.querySelector(".modal-content");
 
 openModalBtn.addEventListener("click", ()=>{
 	modal.classList.remove("hidden");
@@ -10,8 +11,8 @@ closeModal.addEventListener("click", ()=>{
 	modal.classList.add("hidden")
 })
 
-window.addEventListener("click", (e)=>{
-	if(e.target === modal){
-		e.target.classList.add("hidden")
+window.addEventListener("click", (e) => {
+	if (!modalContent.contains(e.target) && !openModalBtn.contains(e.target)) {
+		modal.classList.add("hidden");
 	}
 });
